05ebd6dd6dd590df4362b0b17d1440947a502b89
Brian Hays <bhays7@users.noreply.github.com>
2024-06-05 14:13:00 -0500

    Update Zoom.Zoom.ps1

--- Zoom.Zoom.ps1
+++ Zoom.Zoom.ps1
@@ -24,43 +24,27 @@ Param (
 
 Try {
 
-	Write-Host "Starting"
- 	Write-Host (Get-Location).Path
-
  	If ($DeploymentType -ieq 'Install')
 	{
 
-  		Write-Host "Install Section"
-		Write-Host $PSScriptRoot
-	
-  		Write-Host "Downloading"
 		# Download latest release from Zoom
 		$downloadUri = "https://zoom.us/client/latest/ZoomInstallerFull.msi?archType=x64"
 		$filePath = Join-Path -Path (Get-Location).Path -ChildPath "ZoomInstallerFull.msi"
 		$ProgressPreference = 'SilentlyContinue'
 		Invoke-WebRequest -Uri $downloadUri -Out $filePath -UseBasicParsing
 		
-  		Write-Host "Installing"
 		# Install
 		Start-Process msiexec.exe -Wait -PassThru -ArgumentList "/i ZoomInstallerFull.msi /qn /norestart MSIRestartManagerControl=Disable ZSSOHOST='missouristate' ZoomAutoUpdate=1 ZConfig='kCmdParam_InstallOption=8;EnableEmbedBrowserForSSO=1' ZRecommend='AudioAutoAdjust=1'"
 
-    	Write-Host "Finished"
-  
 	}
 	ElseIf ($DeploymentType -ieq 'Uninstall')
 	{
 		
-  		Write-Host "Uninstall Section"
-  
 		## Uninstall Zoom
 		Install-PackageProvider -Name NuGet -Force | Out-Null
 		Get-Package -Name "Zoom*(64-bit)" -ErrorAction SilentlyContinue | Uninstall-Package
 
-      		Write-Host "Finished"
-
-	}
-
- 	Write-Host "End"
+ 	}
 
 }
 Catch {
