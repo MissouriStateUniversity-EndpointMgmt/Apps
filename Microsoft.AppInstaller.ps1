9cd3be88944e85fc8c7ad1fd3ea5aaddbaff293f
Brian Hays <bhays7@users.noreply.github.com>
2024-06-05 15:37:38 -0500

    Update Microsoft.AppInstaller.ps1

--- Microsoft.AppInstaller.ps1
+++ Microsoft.AppInstaller.ps1
@@ -1,31 +1,49 @@
 <#
 .SYNOPSIS
-	Generic msixbundle GitHub install script
 	
 .DESCRIPTION
-	Downloads and installs msixbundle from GitHub repo
 
+.EXAMPLE
+	
 .NOTES
 	Author:      Brian Hays
-    Created:     2023-09-07
-    Updated:     2024-05-28
+	Created:     2024-06-05
+	Updated:     2024-06-05
 	
-    Version history:
-    1.0.0 - (2023-09-07) - Script created
+	Version history:
+	1.0.0 - (2024-06-05) - Script created
 
 #>
 
-## Download App and Dependencies
+[CmdletBinding()]
+Param (
+	[Parameter(Mandatory=$false)]
+	[ValidateSet('Install')]
+	[string]$DeploymentType = 'Install'
+)
 
-$ProgressPreference = 'SilentlyContinue'
-Invoke-WebRequest -Uri https://aka.ms/getwinget -OutFile Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle
-Invoke-WebRequest -Uri https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx -OutFile Microsoft.VCLibs.x64.14.00.Desktop.appx
-Invoke-WebRequest -Uri https://github.com/microsoft/microsoft-ui-xaml/releases/download/v2.8.6/Microsoft.UI.Xaml.2.8.x64.appx -OutFile Microsoft.UI.Xaml.2.8.x64.appx
+Try {
 
-## Enable Sideloading
-Set-ItemProperty -Path "HKLM:\Software\Policies\Microsoft\Windows\Appx" -Name AllowDevelopmentWithoutDevLicense -Value 1
+ 	If ($DeploymentType -ieq 'Install')
+	{
 
-## Install
-Add-AppxProvisionedPackage -Online -PackagePath Microsoft.VCLibs.x64.14.00.Desktop.appx -SkipLicense -ErrorAction SilentlyContinue
-Add-AppxProvisionedPackage -Online -PackagePath Microsoft.UI.Xaml.2.8.x64.appx -SkipLicense -ErrorAction SilentlyContinue
-Add-AppxProvisionedPackage -Online -PackagePath Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle -SkipLicense -ErrorAction SilentlyContinue
+		## Download App and Dependencies
+		$ProgressPreference = 'SilentlyContinue'
+		Invoke-WebRequest -Uri https://aka.ms/getwinget -OutFile Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle
+		Invoke-WebRequest -Uri https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx -OutFile Microsoft.VCLibs.x64.14.00.Desktop.appx
+		Invoke-WebRequest -Uri https://github.com/microsoft/microsoft-ui-xaml/releases/download/v2.8.6/Microsoft.UI.Xaml.2.8.x64.appx -OutFile Microsoft.UI.Xaml.2.8.x64.appx
+		
+		## Enable Sideloading
+		Set-ItemProperty -Path "HKLM:\Software\Policies\Microsoft\Windows\Appx" -Name AllowDevelopmentWithoutDevLicense -Value 1
+		
+		## Install
+		Add-AppxProvisionedPackage -Online -PackagePath Microsoft.VCLibs.x64.14.00.Desktop.appx -SkipLicense -ErrorAction SilentlyContinue
+		Add-AppxProvisionedPackage -Online -PackagePath Microsoft.UI.Xaml.2.8.x64.appx -SkipLicense -ErrorAction SilentlyContinue
+		Add-AppxProvisionedPackage -Online -PackagePath Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle -SkipLicense -ErrorAction SilentlyContinue
+
+	}
+
+}
+Catch {
+
+}
