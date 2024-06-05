f4c814155c420871cf7e5a1e28a694445e302b81
Brian Hays <bhays7@users.noreply.github.com>
2024-06-05 12:24:26 -0500

    Update Zoom.Zoom.ps1

--- Zoom.Zoom.ps1
+++ Zoom.Zoom.ps1
@@ -1,34 +1,58 @@
+<#
+.SYNOPSIS
+	
+.DESCRIPTION
+
+.EXAMPLE
+	
+.NOTES
+	Author:      Brian Hays
+    Created:     2024-06-05
+    Updated:     2024-06-05
+	
+    Version history:
+    1.0.0 - (2024-06-05) - Script created
+
+#>
+
+[CmdletBinding()]
 Param (
-	[Parameter(Mandatory = $True)]
-	[String]$AppName,
-	[parameter(Mandatory = $True, ParameterSetName = "Install")]
-	[switch]$Install,
-	[parameter(Mandatory = $True, ParameterSetName = "Uninstall")]
-	[switch]$Uninstall
+	[Parameter(Mandatory=$false)]
+	[ValidateSet('Install','Uninstall','Repair')]
+	[string]$DeploymentType = 'Install'
 )
 
-switch ($PSCmdlet.ParameterSetName) {
-	"Install" {
-		Write-Host $AppName
-		Write-Host 'Install'
-	}
-	"Uninstall" {
-		Write-Host $AppName
-		Write-Host 'Uninstall'
-	}
+Try {
+
+    ## Set the script execution policy for this process
+    Try { Set-ExecutionPolicy -ExecutionPolicy 'ByPass' -Scope 'Process' -Force -ErrorAction 'Stop' } Catch {}
+
+    If ($deploymentType -ieq 'Install')
+    {
+
+		# Variables
+		$installArgs = "/i ZoomInstallerFull.msi /qn /norestart MSIRestartManagerControl=Disable ZSSOHOST=""missouristate"" ZoomAutoUpdate=1 ZConfig=""kCmdParam_InstallOption=8;EnableEmbedBrowserForSSO=1"" ZRecommend=""AudioAutoAdjust=1"""
+
+		# Download latest release from Zoom
+		$downloadUri = "https://zoom.us/client/latest/ZoomInstallerFull.msi?archType=x64"
+		$filePath = Join-Path -Path $PSScriptRoot -ChildPath "ZoomInstallerFull.msi"
+		$ProgressPreference = 'SilentlyContinue'
+		Invoke-WebRequest -Uri $downloadUri -Out $filePath -UseBasicParsing
+
+		# Install
+		Start-Process msiexec.exe -Wait -PassThru -ArgumentList $installArgs
+
+    }
+    ElseIf ($deploymentType -ieq 'Uninstall')
+    {
+
+		## Uninstall Zoom
+		Install-PackageProvider -Name NuGet -Force | Out-Null
+		Get-Package -Name "Zoom*(64-bit)" -ErrorAction SilentlyContinue | Uninstall-Package
+
+    }
+
 }
+Catch {
 
-
-# Variables
-# $installArgs = "/i ZoomInstallerFull.msi /q /norestart ZRecommend=""AudioAutoAdjust=1"" ZoomAutoUpdate=""true"" ZoomInstallOption=8 ZSSOHOST=""missouristate"" ZConfig=""EnableEmbedBrowserForSSO=1"""
-$installArgs = "/i ZoomInstallerFull.msi /qn /norestart MSIRestartManagerControl=Disable ZSSOHOST=""missouristate"" ZoomAutoUpdate=1 ZConfig=""kCmdParam_InstallOption=8;EnableEmbedBrowserForSSO=1"" ZRecommend=""AudioAutoAdjust=1"""
-
-# Download latest release from Zoom
-$downloadUri = "https://zoom.us/client/latest/ZoomInstallerFull.msi?archType=x64"
-$filePath = Join-Path -Path $PSScriptRoot -ChildPath "ZoomInstallerFull.msi"
-$ProgressPreference = 'SilentlyContinue'
-Invoke-WebRequest -Uri $downloadUri -Out $filePath -UseBasicParsing
-
-# Install
-$ExitCode = (Start-Process msiexec.exe -Wait -PassThru -ArgumentList $installArgs).ExitCode
-return $ExitCode
+}
