73edcbd920cc92031ccda654a0269064f8f056c5
Brian Hays <bhays7@users.noreply.github.com>
2025-07-11 09:24:19 -0500

    Create Foxit.FoxitEditor.ps1

---
+++ Foxit.FoxitEditor.ps1
@@ -0,0 +1,49 @@
+Write-Output 'File Version 1.00'
+
+function RemoveApp {
+    ## Uninstall Info
+    $UninstallName = "Foxit PDF Editor"
+    $UninstallArgs = "/S"
+    
+    ## Uninstall MSI
+    Install-PackageProvider -Name NuGet -Force | Out-Null
+    Get-Package -Name $UninstallName -ErrorAction SilentlyContinue | Uninstall-Package
+    
+    ## Uninstall EXE
+    (Get-ChildItem -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall","HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall" -ErrorAction SilentlyContinue) | Get-ItemProperty | Where-Object {$_.DisplayName -like $UninstallName} | Foreach { Start-Process $_.UninstallString -Wait -PassThru -ArgumentList $UninstallArgs }
+}
+
+try {
+
+ 	if ($Action -ieq 'Install')
+	{
+        # Remove Old Versions
+        RemoveApp
+
+        ## Release Information
+        $DownloadURI = "https://www.foxit.com/downloads/latest.html?product=Foxit-PDF-Editor-Suite-Pro-Teams&version=&platform=Windows&country=US&language=ML&package_type=msi"
+        Write-Output $DownloadURI
+        $InstallFile = (Invoke-WebRequest -Method Head -Uri $DownloadURI -UseBasicParsing -ErrorAction SilentlyContinue).BaseResponse.ResponseUri.Segments[-1].Replace('%20','')
+
+        # Download new application file
+        $FilePath = Join-Path -Path (Get-Location).Path -ChildPath $InstallFile
+        Write-Output $FilePath
+        $ProgressPreference = 'SilentlyContinue'
+        Invoke-WebRequest -Uri $DownloadURI -Out $FilePath -UseBasicParsing
+
+        ## Install Info
+        $InstallArgs = "/i $FilePath /qn"
+
+        # Install
+        Write-Output 'Install'
+        Start-Process msiexec.exe -Wait -Passthru -ArgumentList $InstallArgs
+ 	}
+	elseif ($Action -ieq 'Remove')
+	{
+		RemoveApp
+ 	}
+
+}
+catch {
+
+}
