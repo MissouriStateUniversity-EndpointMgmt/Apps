d5411d015d91732c8898da37048eb51e0e99dbe5
Brian Hays <bhays7@users.noreply.github.com>
2025-07-11 13:01:53 -0500

    Update Zoom.Workplace.ps1

--- Zoom.Workplace.ps1
+++ Zoom.Workplace.ps1
@@ -1,27 +1,31 @@
+Write-Output 'File Version 1.00'
+
 try {
 
  	if ($Action -ieq 'Install')
 	{
+        ## Release Information
+		$DownloadURI = "https://zoom.us/client/latest/ZoomInstallerFull.msi?archType=x64"
+        Write-Output $DownloadURI
 
-		$installArgs = "/i ZoomInstallerFull.msi /qn /norestart MSIRestartManagerControl=Disable ZSSOHOST=""missouristate"" ZoomAutoUpdate=1 ZConfig=""kCmdParam_InstallOption=8;EnableEmbedBrowserForSSO=1"" ZRecommend=""AudioAutoAdjust=1"""
-
-		# Download latest release
-		$downloadUri = "https://zoom.us/client/latest/ZoomInstallerFull.msi?archType=x64"
-		$filePath = Join-Path -Path (Get-Location).Path -ChildPath "ZoomInstallerFull.msi"
+        # Download new application file
+		$FilePath = Join-Path -Path (Get-Location).Path -ChildPath "ZoomInstallerFull.msi"
+        Write-Output $FilePath
 		$ProgressPreference = 'SilentlyContinue'
-		Invoke-WebRequest -Uri $downloadUri -Out $filePath -UseBasicParsing
+        Invoke-WebRequest -Uri $DownloadURI -Out $FilePath -UseBasicParsing
+
+        ## Install Info
+		$installArgs = "/i $FilePath /qn /norestart MSIRestartManagerControl=Disable ZSSOHOST=""missouristate"" ZoomAutoUpdate=1 ZConfig=""kCmdParam_InstallOption=8;EnableEmbedBrowserForSSO=1"" ZRecommend=""AudioAutoAdjust=1"""
 		
 		# Install
-		Start-Process msiexec.exe -Wait -PassThru -ArgumentList $installArgs
-
+        Write-Output 'Install'
+        Start-Process msiexec.exe -Wait -Passthru -ArgumentList $InstallArgs
 	}
 	elseif ($Action -ieq 'Remove')
 	{
-		
 		## Uninstall
 		Install-PackageProvider -Name NuGet -Force | Out-Null
 		Get-Package -Name "Zoom*(64-bit)" -ErrorAction SilentlyContinue | Uninstall-Package
-
  	}
 
 }
