613fa6588061ef27dd76daf33382ccf075703646
Brian Hays <bhays7@users.noreply.github.com>
2025-07-11 13:35:32 -0500

    Update TechSmith.Camtasia.ps1

--- TechSmith.Camtasia.ps1
+++ TechSmith.Camtasia.ps1
@@ -1,27 +1,31 @@
+Write-Output 'File Version 1.00'
+
 try {
 
  	if ($Action -ieq 'Install')
 	{
+		## Release Information
+		$DownloadURI = "https://download.techsmith.com/camtasiastudio/releases/camtasia.msi"
+		Write-Output $DownloadURI
 
+		# Download new application file
+		$FilePath = Join-Path -Path (Get-Location).Path -ChildPath "camtasia.msi"
+		Write-Output $FilePath
+		$ProgressPreference = 'SilentlyContinue'
+		Invoke-WebRequest -Uri $DownloadURI -Out $FilePath -UseBasicParsing
+
+  		## Install Info
 		$InstallArgs = "/i camtasia.msi TSC_SOFTWARE_KEY=$($ProductKey) TSC_SOFTWARE_USER=""Missouri State University"" TSC_UPDATE_ENABLE=0 /passive /norestart"
 
-		# Download latest release
-		$downloadUri = "https://download.techsmith.com/camtasiastudio/releases/camtasia.msi"
-		$filePath = Join-Path -Path (Get-Location).Path -ChildPath "camtasia.msi"
-		$ProgressPreference = 'SilentlyContinue'
-		Invoke-WebRequest -Uri $downloadUri -Out $filePath -UseBasicParsing
-		
 		# Install
+		Write-Output 'Install'
 		Start-Process msiexec.exe -Wait -PassThru -ArgumentList $installArgs
-
 	}
 	elseif ($Action -ieq 'Remove')
 	{
-		
 		## Uninstall
 		Install-PackageProvider -Name NuGet -Force | Out-Null
 		Get-Package -Name "Camtasia 202*" -ErrorAction SilentlyContinue | Uninstall-Package
-
  	}
 
 }
