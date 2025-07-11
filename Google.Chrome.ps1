97a48ceb7042ff740e4372d2940270fdddd2a57f
Brian Hays <bhays7@users.noreply.github.com>
2025-07-11 13:02:10 -0500

    Create Google.Chrome.ps1

---
+++ Google.Chrome.ps1
@@ -0,0 +1,34 @@
+Write-Output 'File Version 1.00'
+
+try {
+
+ 	if ($Action -ieq 'Install')
+	{
+        ## Release Information
+        $DownloadURI = "https://dl.google.com/dl/chrome/install/googlechromestandaloneenterprise64.msi"
+        Write-Output $DownloadURI
+
+        # Download new application file
+        $FilePath = Join-Path -Path (Get-Location).Path -ChildPath "googlechromestandaloneenterprise64.msi"
+        Write-Output $FilePath
+        $ProgressPreference = 'SilentlyContinue'
+        Invoke-WebRequest -Uri $DownloadURI -Out $FilePath -UseBasicParsing
+
+        ## Install Info
+        $InstallArgs = "/i $FilePath /qn"
+        
+        # Install
+        Write-Output 'Install'
+        Start-Process msiexec.exe -Wait -Passthru -ArgumentList $InstallArgs
+ 	}
+    elseif ($Action -ieq 'Remove')
+    {
+        ## Uninstall
+        Install-PackageProvider -Name NuGet -Force | Out-Null
+        Get-Package -Name "Google Chrome" -ErrorAction SilentlyContinue | Uninstall-Package
+    }
+
+}
+catch {
+
+}
