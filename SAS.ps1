340ba95e6b92cccefe683f3aa055cdb2b5cde8d2
Brian Hays <bhays7@users.noreply.github.com>
2025-11-03 12:45:12 -0600

    Update SAS.ps1

--- SAS.ps1
+++ SAS.ps1
@@ -1,1 +1,23 @@
 
+Write-Output 'File Version 1.00'
+
+$LicenseFile = "SAS94_9D29YD_70085677_Win_X64_Wrkstn.txt"
+
+try {
+
+ 	if ($Action -ieq 'Install')
+	{
+        # Download License File
+        Invoke-WebRequest -Uri "https://api.github.com/repos/MissouriStateUniversity-EndpointMgmt/MCM/contents/$LicenseFile" -Headers @{Authorization = "token $AccessToken"} -OutFile $LicenseFile
+
+ 	}
+    elseif ($Action -ieq 'Remove')
+    {
+        ## Uninstall
+
+    }
+
+}
+catch {
+
+}
