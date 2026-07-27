723ac125b8f30b8e2131290c5f2e72c9e6efb529
Brian Hays <bhays7@users.noreply.github.com>
2026-07-27 12:23:16 -0500

    Update Anthropic.Claude.ps1
    Enable sideloading

--- Anthropic.Claude.ps1
+++ Anthropic.Claude.ps1
@@ -1,22 +1,34 @@
-Write-Output 'File Version 1.00'
+Write-Output 'File Version 1.01'
 
 try {
 	
 	if ($Action -ieq 'Install')
 	{
+
+		## Enable sideloading of trusted MSIX/AppX packages
+		$RegPath = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\AppModelUnlock"
+		# Create the key if it doesn't exist
+		If (!(Test-Path $RegPath)) {
+		    New-Item -Path $RegPath -Force | Out-Null
+		}
+		# Enable sideloading
+		New-ItemProperty -Path $RegPath -Name "AllowAllTrustedApps" -PropertyType DWord -Value 1 -Force | Out-Null
+		Write-Output "Sideloading has been enabled."
+		
 		## Release Information
 		$DownloadURI = "https://claude.ai/api/desktop/win32/x64/msix/latest/redirect"
 		Write-Output $DownloadURI
 
-		# Download new application file
+		## Download new application file
 		$FilePath = Join-Path -Path (Get-Location).Path -ChildPath "Claude.msix"
 		Write-Output $FilePath
 		$ProgressPreference = 'SilentlyContinue'
 		Invoke-WebRequest -Uri $DownloadURI -Out $FilePath -UseBasicParsing
 
-		# Install
+		## Install
 		Write-Output 'Install'
-    Add-AppxProvisionedPackage -Online -PackagePath "Claude.msix" -SkipLicense -Regions "all"
+		Add-AppxProvisionedPackage -Online -PackagePath "Claude.msix" -SkipLicense -Regions "all"
+
 	}
 	elseif ($Action -ieq 'Remove')
 	{
